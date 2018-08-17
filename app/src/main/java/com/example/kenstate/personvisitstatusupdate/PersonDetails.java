1c41155b6ad4ae714851f67b1099ebb91c878f94
muttaisammy <muttaisammy@gmail.com>
2018-08-17 12:18:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kenstate/personvisitstatusupdate/PersonDetails.java
@@ -0,0 +1,16 @@
+package com.example.kenstate.personvisitstatusupdate;
+
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PersonDetails extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+         setContentView(R.layout.activity_person_details);
+    }
+}
